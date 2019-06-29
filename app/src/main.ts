6df5a6629f36fc1e91c9e145445cd4c6af2c9c30
Henning Berge <henning.ber@gmail.com>
2019-06-29 12:43:51 +0200

    added example of FourCC and DecodeFourCC

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,9 +7,17 @@ Log.Init([
 
 function Main(this: void): void {
     Log.Information('Hello World');
+    Log.Information(`${FourCC('I01T')}`);
+    Log.Information(`${DecodeFourCC(FourCC('I01T'))}`);
+
+
 }
 function PrintError(err: any) {
     Log.Fatal(err);
+}
+function DecodeFourCC(fourcc: number): string {
+    // tslint:disable-next-line:no-bitwise
+    return string.char((fourcc >>> 24) & 255, (fourcc >>> 16) & 255, (fourcc >>> 8) & 255, (fourcc) & 255);
 }
 xpcall(() => {
     const init: trigger = CreateTrigger();
