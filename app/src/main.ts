7a077270de34b06b0ad68d2c5932718cdaefacf0
Henning Berge <henning.ber@gmail.com>
2019-07-01 12:09:49 +0200

    fix a bug where it can crash wc3 when selecting the map, then another map

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,29 +1,36 @@
 import { Log, LogLevel } from './lib/Serilog/Serilog';
 import { StringSink } from './lib/Serilog/Sinks/StringSink';
 
-Log.Init([
-    new StringSink(LogLevel.Debug, BJDebugMsg),
-]);
-
-function Main(this: void): void {
-    Log.Information('Hello World');
-    Log.Information(`${FourCC('I01T')}`);
-    Log.Information(`${DecodeFourCC(FourCC('I01T'))}`);
+ceres.addHook('main::after', () => {
 
 
-}
-function PrintError(err: any) {
-    Log.Fatal(err);
-}
-function DecodeFourCC(fourcc: number): string {
-    // tslint:disable-next-line:no-bitwise
-    return string.char((fourcc >>> 24) & 255, (fourcc >>> 16) & 255, (fourcc >>> 8) & 255, (fourcc) & 255);
-}
-xpcall(() => {
-    const init: trigger = CreateTrigger();
-    TriggerRegisterTimerEvent(init, 0, false);
-    TriggerAddAction(init, () => xpcall(() => Main(), err => PrintError(err)));
-},     (err) => {
-    PrintError(err);
+    Log.Init([
+        new StringSink(LogLevel.Debug, BJDebugMsg),
+    ]);
+
+    function Main(this: void): void {
+        Log.Information('Hello World');
+        Log.Information(`${FourCC('I01T')}`);
+        Log.Information(`${DecodeFourCC(FourCC('I01T'))}`);
+
+
+    }
+    function PrintError(err: any) {
+        Log.Fatal(err);
+    }
+    function DecodeFourCC(fourcc: number): string {
+        // tslint:disable-next-line:no-bitwise
+        return string.char((fourcc >>> 24) & 255, (fourcc >>> 16) & 255, (fourcc >>> 8) & 255, (fourcc) & 255);
+    }
+    xpcall(() => {
+        const init: trigger = CreateTrigger();
+        TriggerRegisterTimerEvent(init, 0, false);
+        TriggerAddAction(init, () => xpcall(() => Main(), err => PrintError(err)));
+    },     (err) => {
+        PrintError(err);
+    });
+
 });
+
+
 
